fdbd7d290ae3814a5d77dcb415204bad123ab942
Jonathan Hop <yithril@gmail.com>
2022-08-24 20:48:00 -0400

    initialcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleComponentComponent } from './component/example-component/example-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponentComponent
   ],
   imports: [
     BrowserModule,
