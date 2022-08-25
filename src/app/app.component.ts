a236172ce55b6fe71577bcc4149194b85cd84b66
Jonathan Hop <yithril@gmail.com>
2022-08-24 20:47:55 -0400

    example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Player } from './interfaces/player';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exampleApp';
+
+  counter:number = 0;
+
+  names:string[] = ['bob', 'jane', 'matthew', 'george'];
+
+  price:number = 10.25;
+
+  isValid:boolean = false;
+
+  players:Player[] = [
+    {name: 'Bob', age: 10},
+    {name: 'Steve', age: 15},
+    {name: 'George', age :100}
+  ];
 }
---
+++ src/app/enums/category.ts
@@ -0,0 +1,5 @@
+export enum Category {
+    Breakfast,
+    Lunch,
+    Dinner
+}
